23bf81281d428cfb057fe17f9108d0dab9fb121e
Julián Astudillo <julian12astudillo@gmail.com>
2022-09-29 13:14:57 -0300

    Added all CRUD events

--- src/routes/product.js
+++ src/routes/product.js
@@ -11,4 +11,40 @@ router.post("/products", (req, res) => {
     .catch((error) => res.json({ message: error }));
 });
 
+//get all products
+router.get("/products", (req, res) => {
+  productSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+//get a specific product
+router.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  productSchema
+    .findById(id)
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+//update a product
+router.put("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const { name, price, stock, description } = req.body;
+  productSchema
+    .updateOne({ _id: id }, { $set: { name, price, stock, description } })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
+//delete a product
+router.put("/products/:id", (req, res) => {
+  const { id } = req.params;
+  productSchema
+    .remove({ _id: id })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({ message: error }));
+});
+
 module.exports = router;
