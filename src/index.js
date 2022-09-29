bf557d2b05a90d8f611ead0398d02c64e1650b71
Julián Astudillo <julian12astudillo@gmail.com>
2022-09-29 12:31:59 -0300

    Basic response messages added

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv").config();
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -7,5 +10,13 @@ const PORT = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.send("welcome to my api");
 });
+
+//mongodb connection
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => console.log("🟢 mongodb connection was successful 🟢"))
+  .catch((error) =>
+    console.error("🔴 mongodb connection failed: " + error + " 🔴")
+  );
 
 app.listen(PORT, () => console.log("server listening on port", PORT));
